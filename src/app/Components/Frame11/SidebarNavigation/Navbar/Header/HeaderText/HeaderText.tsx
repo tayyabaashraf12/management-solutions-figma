704e1c7abcfceed7cbbdc23e2f3fa1f864e9072c
abdulmalikKryptoMind <abdul.malik@kryptomind.com>
2025-02-03 09:00:37 +0500

    commit Feb 3/2025(one)

--- src/app/Components/Frame11/SidebarNavigation/Navbar/Header/HeaderText/HeaderText.tsx
+++ src/app/Components/Frame11/SidebarNavigation/Navbar/Header/HeaderText/HeaderText.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const HeaderText = (props: Props) => {
   return (
-    <div className="w-[178px]   h-[20px] font-poppins font-black text-[20px] leading-[20px]  text-[#FFFFFF] ">
+    <div className="w-[178px] capitalize  h-[20px] font-poppins font-black text-[20px] leading-[20px]  text-[#FFFFFF] ">
       tuttle law concil
     </div>
   );
--- src/app/Components/Frame11/SidebarNavigation/Navbar/Navigation/NavItem1/Content/Text/Text.tsx
+++ src/app/Components/Frame11/SidebarNavigation/Navbar/Navigation/NavItem1/Content/Text/Text.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Text = (props: Props) => {
   return (
-    <div className="w-[42px] border border-blue-950  h-[24px] font-[Poppins] font-normal text-[14px] leading-[24px]  text-[#FFFFFF] ">
+    <div className="w-[42px]   h-[24px] font-poppins font-normal text-[14px] leading-[24px]  text-[#FFFFFF] ">
       Home
     </div>
   );
