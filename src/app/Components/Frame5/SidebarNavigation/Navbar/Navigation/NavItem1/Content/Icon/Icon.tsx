c8b08e2df114609891d9e76f366ad90ec471a4ff
tayyabaashraf12 <tayyaba.ashraf@kryptomind.com>
2025-02-25 17:30:50 +0500

    Update home icon svg

--- src/app/Components/Frame5/SidebarNavigation/Navbar/Navigation/NavItem1/Content/Icon/Icon.tsx
+++ src/app/Components/Frame5/SidebarNavigation/Navbar/Navigation/NavItem1/Content/Icon/Icon.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import NavbarHomeIcon from "../../../../../../../../../SVG/home-line.svg";
+import NavbarHomeIcon from "../../../../../../../../SVG/home-line.svg";
 
 const Icon = () => {
   return (
