11fcef4011913981b9d21c77f3e70e8bc741a7fc
tayyabaashraf12 <tayyaba.ashraf@kryptomind.com>
2025-02-26 15:42:22 +0500

    useVoteTokenBalance commeted

--- src/app/utils/VoteTokenBalanceUtils/UseVoteTokenBalance.ts
+++ src/app/utils/VoteTokenBalanceUtils/UseVoteTokenBalance.ts
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
-import connectWallet from "../WalletUtilty";
-import { getVoteTokenBalance } from "./TokenBalanceUtility";
+// import { useEffect, useState } from "react";
+// import connectWallet from "../WalletUtilty";
+// import { getVoteTokenBalance } from "./TokenBalanceUtility";
 
-const useVoteTokenBalance = () => {
-  const [balance, setBalance] = useState<string | null>(null);
+// const useVoteTokenBalance = () => {
+//   const [balance, setBalance] = useState<string | null>(null);
 
-  const fetchVoteTokenBalance = async () => {
-    const userAddress = await connectWallet();
-    console.log("Connected Wallet Address:", userAddress); // Debugging
+//   const fetchVoteTokenBalance = async () => {
+//     const userAddress = await connectWallet();
+//     console.log("Connected Wallet Address:", userAddress); // Debugging
 
-    if (userAddress) {
-      const fetchedBalance = await getVoteTokenBalance(userAddress);
-      console.log("Fetched Balance", fetchedBalance);
-      setBalance(fetchedBalance);
-    }
-  };
+//     if (userAddress) {
+//       const fetchedBalance = await getVoteTokenBalance(userAddress);
+//       console.log("Fetched Balance", fetchedBalance);
+//       setBalance(fetchedBalance);
+//     }
+//   };
 
-  useEffect(() => {
-    fetchVoteTokenBalance();
-  }, []);
+//   useEffect(() => {
+//     fetchVoteTokenBalance();
+//   }, []);
 
-  return balance;
-};
+//   return balance;
+// };
 
-export default useVoteTokenBalance;
+// export default useVoteTokenBalance;
